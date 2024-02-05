6b030e378ba13424c135d5fad0c9d057b1917ea3
barszw <biuro@rsknet.pl>
2024-02-05 10:13:58 +0100

    Set public/private key permissions

--- ssh-keygen.sh
+++ ssh-keygen.sh
@@ -2,17 +2,24 @@
 
 # Abort, no parameter provided
 abort() {
-  echo -e "Abort, no parameter specified, try e.g.:\nsh ssh-keygen.sh id_rsa_backup_via_ssh"
+  echo -e "Abort, no parameter specified, try e.g.:\n---> sh ssh-keygen.sh id_rsa_backup_via_ssh"
   exit 1
 }
 
 # Detecting the user's home directory
 SSH_DIR="$HOME/.ssh"
 
+# Set directory permissions
+chmod 700 $SSH_DIR
+
 [ -z "$1" ] && abort
 echo "-===== Generating Key In: $SSH_DIR/$1 =====-"
 ssh-keygen -t rsa -b 4096 -f "$SSH_DIR/$1" -C "$(whoami)@$(hostname -f)" -q -N ""
 
+# Set public/private key permissions
+chmod 600 $SSH_DIR/$1
+chmod 600 $SSH_DIR/$1.pub
+
 echo "-===== Don't forget to copy the SSH key to your remote server =====-"
 echo "-============ As shown below, this is just an example. ============-"
-echo "ssh-copy-id -i $HOME/.ssh/$1.pub user@host"
+echo "---> ssh-copy-id -i $HOME/.ssh/$1.pub user@host"
