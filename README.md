47dd33d292bb61028af0343e183895666bc40eaf
barszw <biuro@rsknet.pl>
2024-02-06 10:43:41 +0100

    Add clear screen

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 ssh-keygen.sh is a tool for creating new authentication key pairs for SSH. Such key pairs are used for automating logins, single sign-on, and for authenticating hosts.
 
 # Make it executable
-chmod +x ssh-keygen.sh
+chmod u+x ssh-keygen.sh
 
 # Execute scripts
 bash ssh-keygen.sh id_rsa_backup_via_ssh
--- ssh-keygen.sh
+++ ssh-keygen.sh
@@ -1,19 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+function header_info() {
+  clear
+}
+
+header_info
+
 # Bash Color Basics: Coloring Text with Escape Sequences
 source "$(dirname "$0")/colors.ext"
 
 # Abort, no parameter provided
 abort() {
-  echo -e "$PRINT_ERROR No parameter specified, try e.g.:\n$BOLD bash ssh-keygen.sh id_rsa_backup_via_ssh$NO_COLOR"
+  echo -e "$PRINT_ERROR No parameter specified, try e.g.:\n$BOLD bash ssh-keygen.sh id_rsa_backup_via_ssh "$NO_COLOR""
   exit 1
 }
 
-# Detecting the user's home directory
-SSH_DIR="$HOME/.ssh"
 [ -z "$1" ] && abort
 
-echo -e "$PRINT_INFO Generating Key In: $SSH_DIR/$1"
-ssh-keygen -t rsa -b 4096 -f "$SSH_DIR/$1" -C "$(whoami)@$(hostname -f)" -q -N ""
+# Detecting the user's home directory
+SSH_DIR="$HOME/.ssh"
+
+echo -e "$PRINT_INFO Generating Key In: $SSH_DIR/$1 $BOLD"
+ ssh-keygen -t rsa -b 4096 -f "$SSH_DIR/$1" -C "$(whoami)@$(hostname -f)" -q -N ""
 
 # Set directory permissions
 chmod 700 $SSH_DIR
@@ -24,4 +32,4 @@ chmod 644 $SSH_DIR/$1.pub
 
 echo -e "$PRINT_INFO Don't forget to copy the SSH key to your remote server"
 echo -e "$PRINT_INFO As shown below, this is just an example e.g.:"
-echo -e "$BOLD ssh-copy-id -i $HOME/.ssh/$1.pub user@host$NO_COLOR"
+echo -e ""$BOLD"ssh-copy-id -i $HOME/.ssh/$1.pub user@host"$NO_COLOR""
